a37af8ca0d11db9f68be6fd3a70c9bf51531c440
José Padilla <jpadilla@webapplicate.com>
2012-05-01 09:04:37 -0400

    Prepare master view to hold a map and a table view

--- Resources/ui/common/MasterView.js
+++ Resources/ui/common/MasterView.js
@@ -5,6 +5,19 @@ function MasterView() {
 		backgroundColor:'white'
 	});
 
+	var mapview = Titanium.Map.createView({
+	    mapType: Titanium.Map.STANDARD_TYPE,
+	    region: {latitude:33.74511, longitude:-84.38993, 
+	            latitudeDelta:0.01, longitudeDelta:0.01},
+	    animate:true,
+	    regionFit:true,
+	    userLocation:true,
+	    height: 193,
+	    top: 0
+	});
+	
+	self.add(mapview);
+
 	//some dummy data for our table view
 	var tableData = [
 		{title:'Apples', price:'1.25', hasChild:true, color: '#000'},
@@ -14,9 +27,11 @@ function MasterView() {
 		{title:'Pears', price:'1.40', hasChild:true, color: '#000'},
 		{title:'Kiwis', price:'1.00', hasChild:true, color: '#000'}
 	];
-	
+
 	var table = Ti.UI.createTableView({
-		data:tableData
+		data:tableData,
+		height: 225,
+		top: 193
 	});
 	self.add(table);
 	
